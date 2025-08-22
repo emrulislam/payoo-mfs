ce65182a10cdf0f98b11b96d682cf32c3c491b4b
Emrul Islam <emrul.mpl@gmail.com>
2025-08-22 23:35:11 +0600

    added login condition via js and home page for valid credentials

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 document.getElementById("loginButton").addEventListener("click", function (e) {
   e.preventDefault();
-  console.log("login btn clicked");
+  const mobileNumber = 12345678910;
+  const pinNumber = 1234;
+  const mobileNumberValue = parseInt(
+    document.getElementById("mobile-number").value
+  );
+  const pinNumberValue = parseInt(document.getElementById("pin-number").value);
+  if (mobileNumberValue === mobileNumber && pinNumberValue === pinNumber) {
+    window.location.href="./home.html"
+  } else {
+    alert("Invalid Credentials")
+  }
 });
